adf794c3dad154909d96aab955f413ae57573294
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-09-18 23:51:37 +0600

    initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll)
+
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            var img: ImageView = findViewById(R.id.img)
+            val number:TextView = findViewById(R.id.number)
+            toast.show()
+            img.visibility = View.VISIBLE
+            changeImage(img, getDice())
+        }
+    }
+    fun getDice(): Int{
+        return (1..6).random()
+    }
+    fun changeImage(img:ImageView , num:Int){
+        if (num == 1)
+            img.setImageResource(R.drawable.dice1)
+        else if(num == 2)
+            img.setImageResource(R.drawable.dice2)
+        else if(num == 3)
+            img.setImageResource(R.drawable.dice3)
+        else if(num == 4)
+            img.setImageResource(R.drawable.dice4)
+        else if(num == 5)
+            img.setImageResource(R.drawable.dice5)
+        else if(num == 6)
+            img.setImageResource(R.drawable.dice6)
+    }
+}
