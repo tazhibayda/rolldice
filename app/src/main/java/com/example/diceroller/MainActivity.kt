3bc0a3c84caf44dc2788c9d5b3e8c3f2e0b03138
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 08:02:20 +0600

    Adding image to dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,11 +17,25 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            var num:Int = getDice()
+            val img: ImageView = findViewById(R.id.img)
             val number:TextView = findViewById(R.id.number)
+            toast.show()
+            img.visibility = View.VISIBLE
+            val num:Int = getDice()
             number.visibility = View.VISIBLE
             number.text = num.toString()
+            if (num == 1)
+                img.setBackgroundResource(R.drawable.dice1)
+            else if(num == 2)
+                img.setBackgroundResource(R.drawable.dice2)
+            else if(num == 3)
+                img.setBackgroundResource(R.drawable.dice3)
+            else if(num == 4)
+                img.setBackgroundResource(R.drawable.dice4)
+            else if(num == 5)
+                img.setBackgroundResource(R.drawable.dice5)
+            else if(num == 6)
+                img.setBackgroundResource(R.drawable.dice6)
         }
     }
     fun getDice(): Int{
