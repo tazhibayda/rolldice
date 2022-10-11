2d9b51df83231938a0a405107ce39b4449603766
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 07:58:59 +0600

    choose button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll)
+
+        rollButton.setOnClickListener {
+            
+        }
     }
 }
