5b22d8db9a97ce7f77099beb4508b452df06872f
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 08:01:20 +0600

    Fixing getDice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,9 +17,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
+            var num:Int = getDice()
+            val number:TextView = findViewById(R.id.number)
+            number.visibility = View.VISIBLE
+            number.text = num.toString()
         }
     }
     fun getDice(): Int{
-        return (0..6).random()
+        return (1..6).random()
     }
 }
